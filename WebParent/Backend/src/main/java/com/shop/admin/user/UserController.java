48cceace39e1caae4d90a0c4e73793d49942b254
Nazmul Islam <nazmul.rhinewaal@gmail.com>
2021-05-25 22:28:06 +0200

    update

---
+++ WebParent/Backend/src/main/java/com/shop/admin/user/UserController.java
@@ -0,0 +1,14 @@
+package com.shop.admin.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class UserController {
+	
+	@Autowired
+	private UserService userService;
+	
+	
+
+}
