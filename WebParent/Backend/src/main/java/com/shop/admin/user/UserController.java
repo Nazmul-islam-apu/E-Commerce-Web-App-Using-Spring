cf258611de63fdef2fcf124d5b61cd85f0802c5d
Nazmul Islam <nazmul.rhinewaal@gmail.com>
2021-05-27 22:55:27 +0200

    update

--- WebParent/Backend/src/main/java/com/shop/admin/user/UserController.java
+++ WebParent/Backend/src/main/java/com/shop/admin/user/UserController.java
@@ -2,6 +2,7 @@ package com.shop.admin.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class UserController {
@@ -9,6 +10,9 @@ public class UserController {
 	@Autowired
 	private UserService userService;
 	
-	
+	@GetMapping("/users")
+	public String listAll() {
+		return "users";
+	}
 
 }
