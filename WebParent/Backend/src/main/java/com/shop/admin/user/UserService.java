45e71d82e7b4cf4cb25ca5628cad83b3211fc498
Nazmul Islam <nazmul.rhinewaal@gmail.com>
2021-07-29 13:28:10 +0200

    update

--- WebParent/Backend/src/main/java/com/shop/admin/user/UserService.java
+++ WebParent/Backend/src/main/java/com/shop/admin/user/UserService.java
@@ -3,6 +3,7 @@ package com.shop.admin.user;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.shop.common.entity.User;
@@ -16,12 +17,26 @@ public class UserService {
 	@Autowired
 	private RoleRepository roleRepo;
 	
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+	
 	public List<User> listAll(){
 		return (List<User>) repo.findAll();
 	}
 	
 	public List<Role> listRoles(){
 		return (List<Role>) roleRepo.findAll();
+	}
+
+	public void save(User user) {
+		encodePassword(user);
+		repo.save(user);
+		
+	}
+	
+	private void encodePassword(User user) {
+		String encodedPassword = passwordEncoder.encode(user.getPassword());
+		user.setPassword(encodedPassword);
 	}
 
 }
